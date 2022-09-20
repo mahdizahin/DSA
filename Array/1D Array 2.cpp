7fa70499fc7f7408598a0fc42f19bacd1a45292a
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-09-20 21:51:33 +0600

    Add files via upload

---
+++ Array/1D Array 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+
+int arr[]={2,3,5,6,10};
+int sum = 0;
+for(int i=0; i<=4 ; i++){
+
+sum=sum+arr[i];
+
+
+}
+cout<<sum;
+
+}
---
+++ Array/Array1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+cout<<"Data Structure course "<<endl;
+
+int marks []={10,20,25,30,25};
+int i;
+cout<<marks[3]<<endl;
+for(  i=0; i<=4;i++){
+    cout<<marks[i]<<endl;
+}
+}
