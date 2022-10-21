8fc3c3b67dcc883666776d1a0518cb8d5c435742
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-10-22 01:10:37 +0600

    Add files via upload

---
+++ Structure/Structure 1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+struct  {
+int num;
+string name;
+
+}x7;
+x7.num=5;
+cout<<x7.num;
+cout<<"\nInput something ";
+cin>>x7.name;
+
+cout<<x7.name;
+
+}
