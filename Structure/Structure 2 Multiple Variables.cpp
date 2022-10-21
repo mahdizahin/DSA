21ab043f743215d9070c7fa126c633d8650a48d4
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-10-22 01:21:06 +0600

    Add files via upload

---
+++ Structure/Structure 2 Multiple Variables.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+
+struct {
+string name;
+int age;
+
+}first,second,third;
+
+
+first.name="D";
+first.age=19;
+
+second.name="EO";
+second.age=22;
+
+third.name="D";
+third.age=20;
+
+
+cout<<first.name<<" "<<first.age<<endl;
+cout<<second.name<<" "<<second.age<<endl;
+cout<<third.name<<" "<<third.age<<endl;
+
+}
