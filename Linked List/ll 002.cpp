c24c252af3b86e09b0349d149854707387488b70
Mahdi Zahin <103143342+mahdizahin@users.noreply.github.com>
2022-12-08 21:42:42 +0600

    Add files via upload

---
+++ Linked List/ll 002.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node* next;
+
+    node(int val){
+        data=val;
+        next=NULL;
+    }
+
+
+
+};
+
+void add(node* &head, int val){
+
+node* n=new node(val;
+
+node* temp = head;
+
+while(temp->next!=NULL){
+    temp=temp->next;
+}
+
+temo
+}
+
+
+int main(){
+
+
+
+
+return 0;
+}
